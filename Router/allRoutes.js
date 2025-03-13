2b981995b12632536a8c65601f0933b96e336205
Guigui5122 <gpelloin@gmail.com>
2025-03-13 10:09:13 +0100

    ajout page galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "pages/home.html"),
     new Route("404", "Page introuvable", "pages/404.html"),
+    new Route("galerie", "La galerie", "pages/galerie.html"),
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
