f4e71b374c6c99186ee5742c9aef35d7ca98d594
Guigui5122 <gpelloin@gmail.com>
2025-03-17 12:00:19 +0100

    page resa

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,6 +9,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "pages/auth/signup.html"),
     new Route("/account", "Mon compte", "pages/auth/account.html"),
     new Route("/editPassword", "Modification du mot de passe", "pages/auth/editPassword.html"),
+    new Route("/allresa", "Vos réservations", "pages/reservations/allresa.html"),
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
