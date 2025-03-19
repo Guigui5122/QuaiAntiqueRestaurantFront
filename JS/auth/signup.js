e27be57dfb088ffb9d6b2e4e0e2facc50b2a62b9
Guigui5122 <gpelloin@gmail.com>
2025-03-19 18:27:58 +0100

    js confirm pwd

--- JS/auth/signup.js
+++ JS/auth/signup.js
@@ -19,8 +19,10 @@ function validateForm(){
     const nomOk = validateRequired(inputNom);
     const prenomOk = validateRequired(inputPrenom);
     const mailOk = validateMail(inputMail);
+    const passwordOk = validatePassword(inputPassword);
+    const passwordConfirmOk = validateConfirmationPassword(inputPassword, inputValidationPassword);
 
-    if(nomOk && prenomOk && mailOk){
+    if(nomOk && prenomOk && mailOk && passwordOk && passwordConfirmOk){
         btnValidation.disabled = false;
     }
     else{
@@ -55,5 +57,37 @@ function validateMail(input){
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
         return false;
+    }
+}
+// Fonction pour vérifier la conformité du mot de passe 8 huit caracteres avec au moins : 1 minuscule 1 majuscule 1 chiffre et 1 caractere special 
+//regex password : const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+
+function validatePassword(input){
+    // définir le regex
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if(passwordUser.match(passwordRegex)){
+        input.classList.add("is-valid"); // le champ est bien rempli donc valide
+        input.classList.remove("is-invalid"); // le champ est vide donc invalide
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
+}
+
+function validateConfirmationPassword(inputPwd, inputConfirmPwd){
+    if(inputPwd.value == inputConfirmPwd.value){
+        inputConfirmPwd.classList.add("is-valid");
+        inputConfirmPwd.classList.remove("is-invalid");
+        return true;
+    }
+    else{
+        inputConfirmPwd.classList.remove("is-valid");
+        inputConfirmPwd.classList.add("is-invalid");
+        return false;
+        
     }
 }
