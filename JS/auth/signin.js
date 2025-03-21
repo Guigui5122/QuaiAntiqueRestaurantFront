bfaa041e8989f03c127e3ec53fa194389a379654
Guigui5122 <gpelloin@gmail.com>
2025-03-21 09:58:07 +0100

    gestion connexion & cookie

--- JS/auth/signin.js
+++ JS/auth/signin.js
@@ -7,11 +7,9 @@ btnSignin.addEventListener("click", checkCredentials);
 function checkCredentials(){
     // ici il faudra appeler l'API pour vérifier les credentials en BDD, // Email et Mot de passe TEST CONNEXION //
     if(mailInput.value == "test@mail.com" && passwordInput.value =="123"){
-        alert("Vous êtes connecté");
-
         // ici il faudra récupérer le vrai Token
         const token = "vhjjkljhgvjhjklmmjhgcvjkliifhbm!ilyfghuigyhbml,jh";
-
+        setToken(token);
         //placer ce token en cookie
 
         window.location.replace("/"); // redirection vers la page d'accueil si la connexion est ok !
